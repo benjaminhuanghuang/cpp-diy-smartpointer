e946c7e171513401f6e7c0ca6a8e720024e8c56d
benjaminhuanghuang <bhuang@rms.com>
2020-09-29 14:21:22 -0700

    update

--- smart-lazy/smart.h
+++ smart-lazy/smart.h
@@ -11,6 +11,14 @@ class Ptr
   Ptr &operator=(const Ptr &);
 
 public:
+  /*
+    https://www.geeksforgeeks.org/g-fact-93/
+    In C++, if a class has a constructor which can be called with a single argument, 
+    then this constructor becomes conversion constructor because such a constructor 
+    allows conversion of the single argument to the class being constructed.
+    
+    We can avoid such implicit conversions as these may lead to unexpected results. We can make the constructor explicit with the help of explicit keyword. For example, if we try the following program that uses explicit keyword with constructor, we get compilation error.
+  */
   explicit Ptr(T *p = 0) : _ptr(p) {}
 
   ~Ptr()
